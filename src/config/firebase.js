626b5216eb61c0c5629a51481b0c9b4daa4dfdd9
LasseRB <lassebrunt@gmail.com>
2023-02-24 11:26:57 +0100

    chapter 10

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,10 @@
 module.exports = {
 
-  apiKey: VUE_APP_FIREBASE_API_KEY,
-
-  authDomain: VUE_APP_FIREBASE_AUTH_DOMAIN,
-
-  projectId: VUE_APP_FIREBASE_PROJECT_ID,
-
-  storageBucket: VUE_APP_FIREBASE_STORAGE_BUCKET,
-
-  messagingSenderId: VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
-
-  appId:VUE_APP_FIREBASE_APP_ID
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
 
 }
